dbe3f510af131895fbc8c455cfb17bc10ddef5ec
Connor Klopfer <cklopfer10@aol.com>
2018-10-19 11:07:08 -0400

    Added new files. league.py will write a text file now to prevent multiple requests to espn and getting locked out. All teams now have a roster and NFL player reference with even more data on the player. Will plot the league using Dash by Plotly.

---
+++ bin/startup.py
@@ -0,0 +1,57 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from .instance import app
+from bin import (league, graphs)
+from .data import year, swid, espn2
+
+index = html.Div(
+    id = 'main',
+    children = [
+        html.Div(
+            id = 'signIn',
+            children=[
+                dcc.Input(
+                    id='idInput',
+                    placeholder="Enter League Id Here...",
+                    type = 'text'
+                ),
+                html.Button('Submit',
+                           id = 'submitButton')
+            ]
+        )
+    ])
+app.layout = index
+
+myLeague = None
+board = None
+
+
+@app.callback(
+    dash.dependencies.Output('main', 'children'),
+    [dash.dependencies.Input(component_id='submitButton', component_property='n_clicks')],
+        [dash.dependencies.State('idInput', 'value')])
+def buildLeague(n_clicks, value):
+    print(value)
+    if value is not None and n_clicks > 0:
+        myLeague = league.League(year= year,league_id=value, espn_s2=espn2,swid=swid)
+        board = graphs.DashBoard(myLeague)
+        from . import callbacks
+        return board.main_page
+    else:
+        return html.Div(
+            id = 'signIn',
+            children=[
+                dcc.Input(
+                    id='idInput',
+                    placeholder="Enter League Id Here...",
+                    type = 'text'
+                ),
+                html.Button('Submit',
+                           id = 'submitButton')
+            ]
+        )
+
+
+
+
