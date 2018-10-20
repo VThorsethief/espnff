0eb82124c72cd917debfb37411df99387129728e
VTHorsethief <cklopfer10@aol.com>
2018-10-20 10:57:00 -0400

    Made changes to the startup program to include the callbacks in scope. No the graphs wll re-build in response to different user input from the dropdown menu. Uses global variables, but the input is only the drop down menu, so security risks are minimized. The speed is slower, need to consider caching graphs, possibly using session id to get rid of the globals.

--- bin/callbacks.py
+++ bin/callbacks.py
@@ -3,148 +3,148 @@ from dash.dependencies import (Input, Output)
 # from .data import myLeague, board
 #
 # app.layout = board.main_page
-from .instance import app
-from .startup import (myLeague, board)
+from .startup import app
 
+def buildCallbacks(myLeague, board):
+    @app.callback(
+        Output(component_id='leagueDash', component_property='style'),
+        [Input(component_id='dashSelector', component_property='value')]
+    )
+    def update_league_summary(input_value):
+        if input_value != "League Summary":
+            return {'display': 'none'}
+
+    #
+    @app.callback(
+        Output(component_id='teamDash', component_property='style'),
+        [Input(component_id='dashSelector', component_property='value')]
+    )
+    def update_team_layout(input_value):
+        if input_value == "League Summary" or input_value == "League Bubble":
+            return {'display': 'none'}
+
+    @app.callback(
+        Output(component_id='teamScatter', component_property='figure'),
+        [Input(component_id='dashSelector', component_property='value')]
+    )
+    def update_team_data(input_value):
+        if input_value != "League Summary" and input_value != "League Bubble":
+            i = board.index_list.index(input_value)
+            player_total_points = []
+            player_week_points = []
+            player_percent_started = []
+            player_percent_owned = []
+            player_names = []
+            player_position = []
+            for player in myLeague.teams[i].roster:
+                try:
+                    player_total_points.append(player.totalPoints)
+                    player_percent_started.append(player.percentStarted)
+                    player_percent_owned.append(player.percentOwned)
+                    player_names.append(player.firstName + " " + player.lastName)
+                    player_position.append(player.getPosition())
+                    player_week_points.append(player.NFLreference.weekPts)
+                except AttributeError:
+                    player_week_points.append(0)
+                    continue
+            data = [dict(
+                x=player_position,
+                y=player_total_points,
+                z=player_week_points,
+                text=player_names,
+                mode="markers",
+                type='scatter3d',
+
+                marker=dict(
+                    sizemode='diameter',
+                    sizeref=2,
+                    size=player_percent_owned,
+                    color=player_percent_started,
+                    colorscale='Viridis',
+                    colorbar=dict(title='Percent<br>Started')))]
+
+            layout = dict(
+                height=1000,
+                width=1000,
+                title=myLeague.teams[i].owner,
+                scene=dict(
+                    xaxis=dict(
+                        title='Player Position'
+                    ),
+                    yaxis=dict(
+                        title='Total Season Points'
+                    ),
+                    zaxis=dict(
+                        title="Week Points"
+                    )
+                )
+
+            )
+
+            return dict(
+                data=data,
+                layout=layout
+            )
+
+    @app.callback(
+        Output(component_id='teamLine', component_property='figure'),
+        [Input(component_id='dashSelector', component_property='value')]
+    )
+    def update_team_line(input_value):
+        if input_value != "League Summary" and input_value != "League Bubble":
+            i = board.index_list.index(input_value)
+            scatter_list = []
+            for player in myLeague.teams[i].roster:
+                scatter_list.append(
+                    dict(
+                        x=board.week_list,
+                        y=player.scores,
+                        name=player.__repr__(),
+                        mode='lines+markers',
+                        type='scatter'
+                    )
+                )
+            layout = dict(
+                title="Individual Player Progress"
+            )
+            return dict(
+                data=scatter_list,
+                layout=layout
+            )
+
+    @app.callback(
+        Output(component_id='teamBar', component_property='figure'),
+        [Input(component_id='dashSelector', component_property='value')]
+    )
+    def update_team_line(input_value):
+        if input_value != "League Summary" and input_value != "League Bubble":
+            i = board.index_list.index(input_value)
+            barData = [dict(
+                x=["Offensive Power", 'Defensive Power', 'Kicking power'],
+                y=[myLeague.teams[i].offensivePower, myLeague.teams[i].defensivePower,
+                   myLeague.teams[i].kicking_power],
+                type='bar',
+                name=myLeague.teams[i].owner
+            )]
+            layout = dict(
+                title="Distribution of player Scores"
+            )
+            return dict(
+                data=barData,
+                layout=layout
+            )
+
+    @app.callback(
+        Output(component_id='teamHeader', component_property='children'),
+        [Input(component_id='dashSelector', component_property='value')]
+    )
+    def update_team_header(input_value):
+        if input_value != "League Summary" and input_value != "League Bubble":
+            i = board.index_list.index(input_value)
+            return myLeague.teams[i].owner
 
 ## !!!!!  This is where the signin happens, we need to have functioality to import the league and the board
 ## upon instance of the callback, and returs the board main page when the league is built and the board is generated.
 ## The sign in will be the original layout.
 
 # Changing the value of the selection for the main drop down menu.
-@app.callback(
-    Output(component_id='leagueDash', component_property='style'),
-    [Input(component_id = 'dashSelector', component_property = 'value')]
-)
-def update_league_summary(input_value):
-    if input_value != "League Summary":
-        return {'display': 'none'}
-
-#
-@app.callback(
-    Output(component_id='teamDash', component_property='style'),
-    [Input(component_id = 'dashSelector', component_property = 'value')]
-)
-def update_team_layout(input_value):
-    if input_value == "League Summary" or input_value == "League Bubble":
-        return {'display': 'none'}
-
-@app.callback(
-    Output(component_id='teamScatter', component_property='figure'),
-    [Input(component_id = 'dashSelector', component_property = 'value')]
-)
-def update_team_data(input_value):
-    if input_value != "League Summary" and input_value != "League Bubble":
-        i = board.index_list.index(input_value)
-        player_total_points = []
-        player_week_points = []
-        player_percent_started = []
-        player_percent_owned = []
-        player_names = []
-        player_position = []
-        for player in myLeague.teams[i].roster:
-            try:
-                player_total_points.append(player.totalPoints)
-                player_percent_started.append(player.percentStarted)
-                player_percent_owned.append(player.percentOwned)
-                player_names.append(player.firstName + " " + player.lastName)
-                player_position.append(player.getPosition())
-                player_week_points.append(player.NFLreference.weekPts)
-            except AttributeError:
-                player_week_points.append(0)
-                continue
-        data = [dict(
-            x=player_position,
-            y=player_total_points,
-            z=player_week_points,
-            text=player_names,
-            mode="markers",
-            type = 'scatter3d',
-
-            marker=dict(
-                sizemode='diameter',
-                sizeref=2,
-                size=player_percent_owned,
-                color=player_percent_started,
-                colorscale='Viridis',
-                colorbar=dict(title='Percent<br>Started')))]
-
-        layout = dict(
-            height=1000,
-            width=1000,
-            title= myLeague.teams[i].owner,
-            scene = dict(
-                xaxis=dict(
-                    title='Player Position'
-                ),
-                yaxis = dict(
-                    title = 'Total Season Points'
-                ),
-                zaxis = dict(
-                    title = "Week Points"
-                )
-            )
-
-        )
-
-        return dict(
-            data = data,
-            layout = layout
-        )
-
-@app.callback(
-    Output(component_id='teamLine', component_property='figure'),
-    [Input(component_id = 'dashSelector', component_property = 'value')]
-)
-def update_team_line(input_value):
-    if input_value != "League Summary" and input_value != "League Bubble":
-        i = board.index_list.index(input_value)
-        scatter_list = []
-        for player in myLeague.teams[i].roster:
-            scatter_list.append(
-                dict(
-                    x=board.week_list,
-                    y=player.scores,
-                    name=player.__repr__(),
-                    mode='lines+markers',
-                    type = 'scatter'
-                )
-            )
-        layout = dict(
-            title = "Individual Player Progress"
-        )
-        return dict(
-            data = scatter_list,
-            layout = layout
-        )
-
-@app.callback(
-    Output(component_id='teamBar', component_property='figure'),
-    [Input(component_id = 'dashSelector', component_property = 'value')]
-)
-def update_team_line(input_value):
-    if input_value != "League Summary" and input_value != "League Bubble":
-        i = board.index_list.index(input_value)
-        barData = [dict(
-            x = ["Offensive Power", 'Defensive Power', 'Kicking power'],
-            y = [myLeague.teams[i].offensivePower, myLeague.teams[i].defensivePower,
-                                           myLeague.teams[i].kicking_power],
-            type = 'bar',
-            name = myLeague.teams[i].owner
-        )]
-        layout = dict(
-            title = "Distribution of player Scores"
-        )
-        return dict(
-            data = barData,
-            layout = layout
-        )
-
-@app.callback(
-    Output(component_id='teamHeader', component_property='children'),
-    [Input(component_id = 'dashSelector', component_property = 'value')]
-)
-def update_team_header(input_value):
-    if input_value != "League Summary" and input_value != "League Bubble":
-        i = board.index_list.index(input_value)
-        return myLeague.teams[i].owner
